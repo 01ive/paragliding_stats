7f0c6c9eb3fccc379d3a211327798c1a91f1fc61
olive <01ive@free.fr>
2023-08-18 17:47:49 +0200

    refactoring

--- paragliding.py
+++ paragliding.py
@@ -14,93 +14,6 @@ max = lambda a, b : a if a >= b else b
 min = lambda a, b : a if a <= b else b
 
 
-class ParaglidingStats:
-    def __init__(self):
-        self._full_distance_2d = 0
-        self._full_distance_3d = 0
-        self._elevation_loss = 0
-        self._elevation_gain = 0
-        self._max_speed_2d = 0
-        self._max_speed_3d = 0
-        self._max_vert_speed = 0
-        self._min_vert_speed = 0
-        self._speed_2d = list()
-        self._max_average_speed_2d = 0
-        self._bearing = 0
-
-    @property
-    def full_distance_2d(self):
-        return self._full_distance_2d
-    @property
-    def full_distance_3d(self):
-        return self._full_distance_3d
-    @property
-    def elevation_loss(self):
-        return self._elevation_loss
-    @property
-    def elevation_gain(self):
-        return self._elevation_gain
-    @property
-    def max_speed_2d(self):
-        return self._max_speed_2d
-    @property
-    def max_speed_3d(self):
-        return self._max_speed_3d
-    @property
-    def max_vert_speed(self):
-        return self._max_vert_speed
-    @property
-    def min_vert_speed(self):
-        return self._min_vert_speed
-    @property
-    def max_average_speed_2d(self):
-        return self._max_average_speed_2d
-    @property
-    def bearing(self):
-        return self._bearing
-
-    def calculate_distances(self, point_1, point_2):
-        p1 = (point_1.x, point_1.y)
-        p2 = (point_2.x, point_2.y)
-        distance_2d = great_circle(p1, p2).m
-        self._full_distance_2d += distance_2d
-        distance_3d = math.sqrt(distance_2d**2 + (point_1.z - point_2.z)**2)
-        self._full_distance_3d += distance_3d
-
-    def calculate_elevation(self, point_1, point_2):
-        delta_elevation = point_1.z - point_2.z
-        if delta_elevation < 0:
-            self._elevation_gain += abs(delta_elevation)
-        elif delta_elevation > 0:
-            self._elevation_loss += delta_elevation
-
-    def calculate_speed(self, point_1, point_2):
-        p1 = (point_1.x, point_1.y)
-        p2 = (point_2.x, point_2.y)
-        distance_2d = great_circle(p1, p2).m
-        distance_3d = math.sqrt(distance_2d**2 + (point_1.z - point_2.z)**2)
-        delta_elevation = point_1.z - point_2.z
-        self._speed_2d.append(ms_to_kmh(distance_2d / GPS_PERIOD))
-        if len(self._speed_2d) >= AVERAGE_NB_SAMPLE:
-            self._speed_2d.pop(0)
-        average_speed_2d = 0
-        for k in self._speed_2d:
-            average_speed_2d += k
-        average_speed_2d /= len(self._speed_2d)
-        self._max_average_speed_2d = max(self._max_average_speed_2d, average_speed_2d)
-        self._max_speed_2d = max(self._max_speed_2d, ms_to_kmh(distance_2d / GPS_PERIOD))
-        self._max_speed_3d = max(self._max_speed_3d, ms_to_kmh(distance_3d / GPS_PERIOD))
-        self._max_vert_speed = max(self._max_vert_speed, delta_elevation)
-        self._min_vert_speed = min(self._min_vert_speed, delta_elevation)
-
-    def calculate_bearing(self, point_1, point_2):
-        bearing = math.atan2(math.sin(point_2.x - point_1.x) * math.cos(point_2.y),
-                                math.cos(point_1.y) * math.sin(point_2.y) - 
-                                math.sin(point_1.y) * math.cos(point_2.y) * math.cos(point_2.x - point_1.x)
-                                )
-        self._bearing = math.degrees(bearing)
-
-
 class ParaglidingPoint(pygeoif.geometry.Point):
     def __init__(self, *args):
         super().__init__(*args)
@@ -214,6 +127,19 @@ class ParaglidingLine(pygeoif.geometry.LineString):
             min_vertical_speed = min(min_vertical_speed, point.vertical_speed)
         return min_vertical_speed
     
+    def average_speed_2d(self):
+        average_speed_2d = 0
+        for point in self.paraliding_geoms:
+            average_speed_2d += point.speed_2d
+        average_speed_2d /= len(self.paraliding_geoms)
+        return average_speed_2d
+    
+    def average_speed_3d(self):
+        average_speed_3d = 0
+        for point in self.paraliding_geoms:
+            average_speed_3d += point.speed_3d
+        average_speed_3d /= len(self.paraliding_geoms)
+        return average_speed_3d
 
 
 class ParaglidingKML(kml.KML):
@@ -252,43 +178,13 @@ class ParaglidingKML(kml.KML):
     def read_trace(self):
         for item in self._items:
             if isinstance(item.geometry, pygeoif.geometry.LineString):
-                # self._current_line = item.geometry
                 self._current_line = ParaglidingLine(item.geometry.coords)
                 break
         print(self._current_line)
 
-    def process_stats(self):
-        stats = ParaglidingStats()
-        for i, point in enumerate(self._current_line.geoms):
-            if i == len(self._current_line.geoms)-1:
-                break
-            next_point = self._current_line.geoms[i+1]
-            # distances
-            stats.calculate_distances(point, next_point)
-            # elevation
-            stats.calculate_elevation(point, next_point)
-            # speed
-            stats.calculate_speed(point, next_point)
-            # bearing
-            stats.calculate_bearing(point, next_point)
-            
-        return stats
-
 
 if __name__ == "__main__":
     my_kml = ParaglidingKML(kml_file)
-
-    stats = my_kml.process_stats()
-    
-    print("My stats:")
-    print("Distance 2d: " + str(stats.full_distance_2d) + " m")
-    print("Distance 3d: " + str(stats.full_distance_3d) + " m")
-    print("Elevation gain: " + str(stats.elevation_gain) + " m")
-    print("Elevation loss: " + str(stats.elevation_loss) + " m")
-    print("Max speed 2d: " + str(stats.max_speed_2d) + " km/h")
-    print("Max speed 3d: " + str(stats.max_speed_3d) + " km/h")
-    print("Max vertical speed: " + str(stats.max_vert_speed) + " m/s")
-    print("Min vertical speed: " + str(stats.min_vert_speed) + " m/s")
 
     date = datetime.date.fromisoformat(my_kml.items[1].name)
     time = datetime.time.fromisoformat(my_kml.items[0].name[-6:-1])
@@ -308,21 +204,9 @@ if __name__ == "__main__":
     print("Elevation high: " + str(my_kml._current_line.high()) + " m")
     print("Max speed 2d: " + str(my_kml._current_line.max_speed_2d()) + " km/h")
     print("Max speed 3d: " + str(my_kml._current_line.max_speed_3d()) + " km/h")
+    print("Average speed 2d: " + str(my_kml._current_line.average_speed_2d()) + " km/h")
+    print("Average speed 3d: " + str(my_kml._current_line.average_speed_3d()) + " km/h")
     print("Max vertical speed: " + str(my_kml._current_line.max_vertical_speed()) + " m/s")
     print("Min vertical speed: " + str(my_kml._current_line.min_vertical_speed()) + " m/s")
-    
-    
-    
+        
     pass
-
-    # my_d = datetime.date.fromisoformat(my_kml.items[1].name)
-
-    # t = my_kml.items[0].name
-    # my_t = datetime.time.fromisoformat(t[-6:-1])
-
-    # my_dt = datetime.datetime.combine(my_d, my_t)
-
-    # delta = int(my_kml.items[2].name[-5])
-    # my_delta = datetime.timedelta(seconds=delta)
-
-    # my_delta.seconds
